9cd0abb68dc378a3d2cd8d2925dd59a603596275
Nhanoccho <nhaannguyen16@gmail.com>
2026-09-17 10:58:30 +0700

    chore: remove Pages functions to allow purely static zero-config deploy

--- functions/api/[[route]].ts
+++
@@ -1,6 +0,0 @@
-import app from '../../backend/src/index';
-
-// Native Pages Functions handler using Hono's universal app.fetch
-export const onRequest = async (context: any) => {
-  return app.fetch(context.request, context.env, context);
-};
